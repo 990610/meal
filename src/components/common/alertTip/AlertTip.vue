f547127e8e74443e3dec714a9cafc423db347617
piyuanxing <1611828518@qq.com>
2021-03-16 21:56:18 +0800

    meal

---
+++ src/components/common/alertTip/AlertTip.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="alert-tip">
+    <div class="fixd"></div>
+    <div class="box">
+      <div class="title">{{title}}</div>
+      <span @click="backClick" class="back">返回</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AlertTip',
+  props: {
+    title: {
+      type: String,
+      default: '登录成功'
+    }
+  },
+  methods: {
+    backClick() {
+      this.$emit('backClick')
+    }
+  }
+}
+</script>
+<style scoped>
+  .fixd {
+    width: 180px;
+    height: 180px;
+    position: absolute;
+    left: 0;
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    margin: auto auto;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #9370db;
+    animation: run 2s linear infinite;
+  }
+  .fixd::after {
+    content: '';
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    right: 5px;
+    bottom: 5px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    animation: run 3s linear infinite;
+    border-top-color: #ba55d3;
+  }
+  .fixd::before {
+    content: '';
+    position: absolute;
+    top: 15px;
+    left: 15px;
+    right: 15px;
+    bottom: 15px;
+    border-radius: 50%;
+    border: 3px solid transparent;
+    border-top-color: #f0f;
+    animation: run 1.5s linear infinite;
+  }
+  /* 定义旋转动画 */
+  @keyframes run {
+    0% {
+      transform: rotate(0);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+  /* 定义缩放 */
+  .box {
+    font-size: 18px;
+    width: 120px;
+    height: 120px;
+    background-color: rgb(219, 52, 219);
+    opacity: 0.3;
+    position: absolute;
+    border-radius: 50%;
+    left: 0px; 
+    right: 0px;
+    top: 0px;
+    bottom: 0px;
+    margin: auto auto;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    white-space:normal;
+    text-align: center;
+    /* overflow: hidden; */
+  }
+  .title {
+    margin: 10px auto;
+  }
+  .back {
+    margin: 10px auto;
+  }
+  .alert-tip {
+    height: 100vh;
+    background-color: #222;
+  }
+  
+
+</style>
--- src/views/login/Login.vue
+++ src/views/login/Login.vue
@@ -1,61 +1,128 @@
 <template>
-  <div class="Login">
-    <div class="iconfont" @click="$router.back()">&#xe604;</div>
-
-    <div class="title">老尹外卖</div>
-    <div class="body">
-      <div class="type">
-        <div class="dl" @click="typeClick('message')">短信登录
-          <div v-if="sign == 'message'" class="line"></div>
+  <div class="login">
+    <div v-if="enter">
+      <!-- 标题 -->
+      <div class="iconfont" @click="$router.back()">&#xe604;</div>
+      <div class="title">饿了么</div>
+      <!-- 两种登录 -->
+      <div class="body">
+        <div class="type">
+          <div class="dl" @click="typeClick('message')">短信登录
+            <div v-if="sign == 'message'" class="line"></div>
+          </div>
+          <div class="dl" @click="typeClick('number')">密码登录
+            <div v-if="sign == 'number'" class="line"></div>
+          </div>
         </div>
-        <div class="dl" @click="typeClick('number')">密码登录
-          <div v-if="sign == 'number'" class="line"></div>
-        </div>
-      </div>
       <!-- 短信登录方式 -->
-      <div v-if="sign == 'message'" class="message-number">
-        <input type="text" placeholder="手机号" maxlength="11" v-model="phone">
-        <input type="text" placeholder="验证码" maxlength="6">
+        <div v-if="sign == 'message'" class="message-number" key="message">
+          <input type="text" placeholder="手机号" maxlength="11" v-model="phone">
+          <input type="text" placeholder="验证码" maxlength="6" v-model="code">
         <div>
-          <input type="submit" :disabled='!rightPhone' value="获取验证码" style="backgroundColor:white;border: 0px;outline:none" >
-          <span>88888</span>
+          <input type="submit" 
+            :disabled='!rightPhone' 
+            :value="this.time>0 ? '已发送'+'('+(this.time)+'s'+')' :'获取验证码'" 
+            @click="identifyClick" 
+            style="backgroundColor:white;border: 0px;outline:none" >
+            <span></span>
+        </div>
+        </div>
+      <!-- 密码登录方式 -->
+      <!-- if是条件渲染，并且会复用已有元素，可以通过key来表示完全独立，进而不复用。不频繁切换时使用 -->
+      <!-- show是直接渲染，存在，只是简单的基于css切换，频繁切换时使用 -->
+        <div v-if="sign == 'number'" class="pass-number" key="number">
+          <input type="text" placeholder="手机号/邮箱/用户名" maxlength="18" v-model="email">
+          <input type="password" placeholder="密码" v-model="password">
+          <div class="yzstyle">
+            <input type="text" placeholder="验证码" style="border:0px;outline:none">
+            <span @click="yzClick">sdfsdf</span>
+          </div>
         </div>
       </div>
-      <!-- 密码登录方式 -->
-      <div v-if="sign == 'number'" class="pass-number">
-        <input type="text" placeholder="账号">
-        <input type="text" placeholder="密码">
-        <input type="text" placeholder="验证码">
-        <div>验证码 <span>sdfsdf</span></div>
+      <div class="point">温馨提示: 未注册老阴外卖账号的手机号，登录时将自动注册，且代表已经同意<span>《用户注册协议》</span> </div>
+      <div class="btn">
+        <button @click="enterClick">登录</button>
       </div>
     </div>
-    <div class="point">温馨提示: 未注册老阴外卖账号的手机号，登录时将自动注册，且代表已经同意<span>《用户注册协议》</span> </div>
-    <div class="btn">
-      <button>登录</button>
-    </div>
+    <!-- 警告框组件 -->
+    <alert-tip v-else :title="errorWay" @backClick='backClick'></alert-tip>
   </div>
 </template>
 <script>
+import AlertTip from '../../components/common/alertTip/AlertTip.vue'
 export default {
+  components: { AlertTip },
   name: 'Login',
   data() {
     return {
-      // 用于标识短信和密码登录的样式选择
+      // 用于标识短信和密码登录的样式选择,message表示短信登录，number表示密码登录
       sign: 'message',
-      // v-model双向绑定，可以动态拿到input输入的值保存到phone中
-      phone: '',
+      //短信验证码计时的时间
+      time: 0, 
+      // 登录界面与登录警告界面的切换
+      enter: true,
+      // 传入警告组件的信息
+      errorWay: '手机号码错误',
+      //以下几个都是 v-model双向绑定，可以动态拿到input输入的值保存
+      phone: '', //手机号
+      code: '', // 短信验证码，
+      email:'', // 密码登录手机，邮箱
+      password:'', // 密码
+      captcha:'' //图形验证码
+      
     }
   },
   computed: {
     rightPhone() {
       return /^1([38][0-9]|4[579]|5[0-3,5-9]|6[6]|7[0135678]|9[89])\d{8}$/.test(this.phone)
       // return /^1\d{10}$/.test(this.phone)
-    }
+    },
   },
   methods: {
-    // 点击相关
+    /* 点击相关*/
+
+    yzClick() {
+      // 图片验证码
+      console.log('---')
+    },
     typeClick(item) {
+      // 点击切换登录方式
       this.sign = item
+    },
+    // 点击获取短信验证码,
+    identifyClick() {
+      // 只有为0时，才能点击。
+      if(this.time == 0) {
+        this.time = 30;
+        const timer = setInterval(() => {
+          this.time--;
+          if(this.time<=0) {
+            clearInterval(timer)
+          }
+        },1000);
+        // 发送请求(向指定手机发送验证码短信)
+      }
+    },
+    //点击登录进行前台验证
+    enterClick() {
+      this.enter = false;
+      if(/^1\d{10}$/.test(this.phone)) {
+        // console.log('手机号正确')
+        if(/\d{6}$/.test(this.code)) {
+          // console.log('验证码正确')
+          return this.errorWay = '登录成功'
+        }else {
+          // console.log('验证码不正确')
+          return this.errorWay = '验证码不正确'
+        }
+      }else{
+        // console.log('手机号不正确')
+        return this.errorWay = '手机号不正确'
+      }
+    },
+    // 监听子组件的点击，返回登录界面
+    backClick() {
+      this.enter = true
     }
   }
 }
@@ -64,6 +131,9 @@ export default {
 @import '../../assets/img/左箭头&#xe604;/iconfont.css';
   .login {
     overflow: hidden;
+    /* position: relative; */
+    /* display: flex; */
+    /* justify-content: center; */
   }
   .iconfont {
     font-size: 30px;
@@ -99,17 +169,23 @@ export default {
     /* border: 1px solid black; */
     margin: 20px auto;
   }
-  .message-number input, .pass-number input{
+  .message-number input, .pass-number input {
     width: 75%;
-    height: 25px;
+    height: 33px;
     margin: 5px auto;
     border-radius: 8px;
+    /* border:1px solid blue */
   }
+
   .message-number div,.pass-number div {
     width: 75%;
     color: coral;
     margin: 5px auto;
     font-size: 14px;
+  }
+  .yzstyle {
+    width: 75%;
+    /* border: 1px solid black; */
   }
   .point {
     /* border: 1px solid black; */
